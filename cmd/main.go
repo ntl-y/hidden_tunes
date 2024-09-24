26c95bc10b9b7da746e46df4a38737ace9d215af
ntl-y <n-danilkina@list.ru>
2024-09-24 22:50:59 +0000

    jamendo

--- cmd/main.go
+++ cmd/main.go
@@ -1,114 +1,73 @@
 package main
 
 import (
-	"context"
-	"encoding/json"
 	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
 	"net/url"
 	"os"
-	"os/user"
-	"path/filepath"
 
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/google"
-	"google.golang.org/api/youtube/v3"
+	"github.com/joho/godotenv"
+	"github.com/sirupsen/logrus"
 )
 
-func tokenCacheFile() (string, error) {
-	usr, err := user.Current()
-	if err != nil {
-		return "", err
-	}
-	tokenCacheDir := filepath.Join(usr.HomeDir, ".credentials")
-	os.MkdirAll(tokenCacheDir, 0700)
-	return filepath.Join(tokenCacheDir,
-		url.QueryEscape("hidden_tunes.json")), err
+const (
+	version = "v3.0"
+	entity
+	subentity
+)
+
+func gatherParams() url.Values {
+	params := url.Values{}
+	return params
+
 }
 
-func tokenFromFile(file string) (*oauth2.Token, error) {
-	f, err := os.Open(file)
+func createRequest() *http.Request {
+	id := os.Getenv("CLIENT_ID")
+	secret := os.Getenv("CLIENT_SECRET")
+	params := gatherParams().Encode()
+
+	url := fmt.Sprintf("https://api.jamendo.com/%s/%s/%s/?%s", version, entity, subentity, params)
+
+	req, _ := http.NewRequest("POST", url, nil)
+	req.Header.Add("Authorization", bearer)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	return req
+}
+
+func sendRequest(client *http.Client, req *http.Request) (*http.Response, error) {
+	resp, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
-	t := &oauth2.Token{}
-	err = json.NewDecoder(f).Decode(t)
-	defer f.Close()
-	return t, err
-}
-
-func getTokenFromWeb(config *oauth2.Config) *oauth2.Token {
-	authURL := config.AuthCodeURL("state-token", oauth2.AccessTypeOffline)
-	fmt.Printf("Go to the following link in your browser then type the "+
-		"authorization code: \n%v\n", authURL)
-
-	var code string
-	if _, err := fmt.Scan(&code); err != nil {
-		log.Fatalf("Unable to read authorization code %v", err)
-	}
-
-	tok, err := config.Exchange(oauth2.NoContext, code)
-	if err != nil {
-		log.Fatalf("Unable to retrieve token from web %v", err)
-	}
-	return tok
-}
-
-func saveToken(file string, token *oauth2.Token) {
-	fmt.Printf("Saving credential file to: %s\n", file)
-	f, err := os.OpenFile(file, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0600)
-	if err != nil {
-		log.Fatalf("Unable to cache oauth token: %v", err)
-	}
-	defer f.Close()
-	json.NewEncoder(f).Encode(token)
-}
-
-func getClient(ctx context.Context, config *oauth2.Config) *http.Client {
-	cacheFile, err := tokenCacheFile()
-	if err != nil {
-		log.Fatalf("Unable to get path to cached credential file. %v", err)
-	}
-	tok, err := tokenFromFile(cacheFile)
-	if err != nil {
-		tok = getTokenFromWeb(config)
-		saveToken(cacheFile, tok)
-	}
-	return config.Client(ctx, tok)
-}
-
-func handleError(err error, message string) {
-	if message == "" {
-		message = "Error making API call"
-	}
-	if err != nil {
-		log.Fatalf(message+": %v", err.Error())
-	}
-}
-
-func videoListByViewsCount(service *youtube.Service) {
-
+	return resp, nil
 }
 
 func main() {
-	ctx := context.Background()
-
-	b, err := ioutil.ReadFile("client_secret.json")
-	if err != nil {
-		log.Fatalf("Unable to read client secret file: %v", err)
+	logrus.SetFormatter(&logrus.JSONFormatter{})
+	if err := godotenv.Load(); err != nil {
+		logrus.Fatalln(err)
 	}
 
-	config, err := google.ConfigFromJSON(b, youtube.YoutubeReadonlyScope)
+	client := &http.Client{}
+	req := createRequest()
+	resp, err := sendRequest(client, req)
 	if err != nil {
-		log.Fatalf("Unable to parse client secret file to config: %v", err)
+		logrus.Println(err)
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Println(err)
 	}
 
-	client := getClient(ctx, config)
-	service, err := youtube.New(client)
+	err = os.WriteFile("response.json", body, 0644)
+	if err != nil {
+		logrus.Fatalln(err)
+	}
 
-	handleError(err, "Error creating YouTube client")
-
-	videoListByViewsCount(service)
+	fmt.Println("Response saved to response.json")
 }
